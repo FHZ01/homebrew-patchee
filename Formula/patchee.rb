02c4aa9d86784cfe56211f5b8e679ab4534280d4
FHZ01 <faisal@patchwork.health>
2025-01-03 18:18:34 +0000

    Update formula to use correct path

--- Formula/patchee.rb
+++ Formula/patchee.rb
@@ -2,11 +2,13 @@ class Patchee < Formula
   desc "CLI tool for AWS EC2 instance management and SSH connections"
   homepage "https://github.com/FHZ01/patchee"
   head "https://github.com/FHZ01/patchee.git", branch: "main"
-
+  
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    cd "cmd/patchee" do  # Change to the directory containing main.go
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+    end
   end
 
   test do
